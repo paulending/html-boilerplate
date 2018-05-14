08ee0cf8d673822afb0f7a66c258dd94b9c345d1
Pavel Konecny <pavel.konecny@alza.cz>
2018-05-14 11:43:11 +0200

    initial project structure and configs

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # html-boilerplate
+
+Folders:
+dist - contain final compiled code
+src - contain all source files
+
+Prerequisites:
+Git (https://git-scm.com/)
+NodeJs (https://nodejs.org/en/)
+Gulp (https://gulpjs.com/)
+Sass (https://sass-lang.com/)
+Less (http://lesscss.org/)
+
+Usage:
+Clone this repository:
+git clone https://github.com/paulending/html-boilerplate.git
+
+Instal nodejs packages:
+npm install
+'run command in cloned repository folder'
+
+Prepare your html, less, sass code and run gulp tasks:
+
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+
+var src = './src';
+var dest = './app';
+
+// TODO: minify js
+// TODO: move and optimize images
+// TODO: add css autoprefixer
+
+/// Compile SASS
+gulp.task('sass', function() {
+    return gulp.src(src + '/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(dest + '/css'))
+        .pipe(browserSync.stream());
+});
+
+/// Compile LESS
+gulp.task('less', function() {
+    return gulp.src(src + '/less/**/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(dest + '/css'))
+        .pipe(browserSync.stream());
+});
+
+/// Watch styles and html
+gulp.task('watch', function() {
+    gulp.watch(src + '/sass/**/*.scss', ['sass']);
+    gulp.watch(src + '/less/**/*.less', ['less']);
+    gulp.watch(src + "/*.html");
+});
+
+/// Browsersync
+// Static Server + watching less/scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch(src + "/scss/*.scss", ['sass']);
+    gulp.watch(src + "/less/*.less", ['less']);
+    gulp.watch(src + "/*.html").on('change', browserSync.reload);
+});
+
+/// Gulp tasks
+gulp.task('default', ['serve']); // run browsersync and watch sources
